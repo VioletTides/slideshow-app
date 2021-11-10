a529ec8fb658df24479a6aec4a5001b00bfb9896
Muffinlord45 <81711759+Muffinlord45@users.noreply.github.com>
2021-11-10 15:43:20 -0500

    made changes

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import os
+print(os.getcwd())
+directory = str(input("Please enter in the directory you wish to create a slideshow with, eg. user/desktop/folder: "))
 
+while os.path.exists(directory) == False:
+    directory = str(input("Invalid directory. Please try again: "))
+
+for file in os.scandir(directory):
+    if file.is_file():
+        print(file.path)
+    # if os.path.isfile(currentFile):
+    #     print(currentFile)

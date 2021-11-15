66a9a3b3af1c7278e7c873249bb7d30d2fcb2c1f
Muffinlord45 <81711759+Muffinlord45@users.noreply.github.com>
2021-11-15 17:35:21 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import os
+from pyglet import image
 print(os.getcwd())
 directory = str(input("Please enter in the directory you wish to create a slideshow with, eg. user/desktop/folder: "))
 
@@ -8,5 +9,8 @@ while os.path.exists(directory) == False:
 for file in os.scandir(directory):
     if file.is_file():
         print(file.path)
+    if file.endswith("jpeg"):
+        pic = image.load(file)
+        pic.blit(0,0,0)
     # if os.path.isfile(currentFile):
     #     print(currentFile)
